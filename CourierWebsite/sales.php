f6835448236c1f0307f80a6d9f66cbc53eb4a12c
GauravvBaral <gauravbaral177@gmail.com>
2025-03-21 02:43:39 +0545

    Update sales.php

--- CourierWebsite/sales.php
+++ CourierWebsite/sales.php
@@ -46,34 +46,42 @@ $results = mysqli_query($db, "SELECT * FROM orders");
             </tr>
         </thead>
         <tbody>
-            <?php while ($row = mysqli_fetch_array($results)) { ?>
-                <tr>
-                    <td><?php echo $row['name']; ?></td>
-                    <td><?php echo $row['address']; ?></td>
-                    <td><?php echo $row['phone']; ?></td>
-                    <td><?php echo $row['customer_email']; ?></td>
-                    <td><?php echo $row['toname']; ?></td>
-                    <td><?php echo $row['toaddress']; ?></td>
-                    <td><?php echo $row['tophone']; ?></td>
-                    <td><?php echo $row['weight']; ?></td>
-                    <td><?php echo $row['time']; ?></td>
-                    <td><?php echo $row['status']; ?></td>
-                    <td><?php echo $row['delivery_received_status']; ?></td>
-                    <td><?php echo $row['delivery_delivered_status']; ?></td>
-                    <td><?php echo $row['received_time']; ?></td>
-                    <td><?php echo $row['delivery_time']; ?></td>
-                    <td>
-                        <a class="approve_btn" href="config.php?approve=<?php echo $row['id']; ?>">Approve</a>
-                    </td>
-                    <td>
-                        <a class="deny_btn" href="config.php?declined=<?php echo $row['id']; ?>">Decline</a>
-                    </td>
-                    <td>
-                        <a class="del_btn" href="config.php?del=<?php echo $row['id']; ?>">Delete</a>
-                    </td>
-                </tr>
-            <?php } ?>
-        </tbody>
+    <?php while ($row = mysqli_fetch_array($results)) { ?>
+        <tr>
+            <td><?php echo $row['name']; ?></td>
+            <td><?php echo $row['address']; ?></td>
+            <td><?php echo $row['phone']; ?></td>
+            <td><?php echo $row['customer_email']; ?></td>
+            <td><?php echo $row['toname']; ?></td>
+            <td><?php echo $row['toaddress']; ?></td>
+            <td><?php echo $row['tophone']; ?></td>
+            <td><?php echo $row['weight']; ?></td>
+            <td><?php echo $row['time']; ?></td>
+            <td><?php echo $row['status']; ?></td>
+            <td><?php echo $row['delivery_received_status']; ?></td>
+            <td><?php echo $row['delivery_delivered_status']; ?></td>
+            <td><?php echo $row['received_time']; ?></td>
+            <td><?php echo $row['delivery_time']; ?></td>
+            <td>
+                <?php if ($row['status'] !== "cancelled"): ?>
+                    <a class="approve_btn" href="config.php?approve=<?php echo $row['id']; ?>">Approve</a>
+                <?php else: ?>
+                    <button class="approve_btn" disabled>Approve</button>
+                <?php endif; ?>
+            </td>
+            <td>
+                <?php if ($row['status'] !== "cancelled"): ?>
+                    <a class="deny_btn" href="config.php?declined=<?php echo $row['id']; ?>">Decline</a>
+                <?php else: ?>
+                    <button class="deny_btn" disabled>Decline</button>
+                <?php endif; ?>
+            </td>
+            <td>
+                <a class="del_btn" href="config.php?del=<?php echo $row['id']; ?>">Delete</a>
+            </td>
+        </tr>
+    <?php } ?>
+</tbody>
     </table>
     <div style="text-align: center; margin-top: 20px;">
         <button onclick="window.location.href='admindash.php'" class="hover-button">
