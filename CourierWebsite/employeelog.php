fcf71dfe4eab113c9a57312ae602e78a919e7529
GauravvBaral <gauravbaral177@gmail.com>
2025-03-21 22:33:14 +0545

    Update employeelog.php

--- CourierWebsite/employeelog.php
+++ CourierWebsite/employeelog.php
@@ -1,24 +1,22 @@
 <?php
 session_start();
-include('emplogin.php'); // Include the database connection file
+include('emplogin.php'); 
 
 if (isset($_POST['email']) && isset($_POST['password'])) {
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $password = $_POST['password'];
 
-    // Fetch employee data
     $query = "SELECT * FROM employee WHERE email='$email'";
     $result = mysqli_query($conn, $query) or die("Could not execute query: " . mysqli_error($conn));
     $row = mysqli_fetch_assoc($result);
 
-    // Verify password
     if ($row && password_verify($password, $row['password'])) {
-        $_SESSION['username'] = $row['username']; // Set session variable for username
-        $_SESSION['password'] = $password; // Set session variable for password
-        header('Location: editbooking.php'); // Redirect to employee dashboard
+        $_SESSION['username'] = $row['username']; 
+        $_SESSION['password'] = $password; 
+        header('Location: editbooking.php'); 
         exit();
     } else {
-        header("Location: employeelog.php?error=invalid_credentials"); // Redirect with error
+        header("Location: employeelog.php?error=invalid_credentials"); 
         exit();
     }
 }
@@ -52,17 +50,6 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
             <div class="textbox">
                 <i class="fa fa-lock" aria-hidden="true"></i>
                 <input type="password" placeholder="Password" name="password" required>
-            </div>
-            <div>
-                <tr>
-                    <td align="center" colspan="2">
-                        <div class="login-section">
-                            <a href="employee.html">
-                                <span style="color: blue;">Sign Up/Register</span>
-                            </a>
-                        </div>
-                    </td>
-                </tr>
             </div>
             <div>
                 <input class="btn" type="submit" name="" value="Sign in">
